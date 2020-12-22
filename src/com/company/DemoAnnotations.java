9e7ad2ca41d80d4ea02ef42eef22fb425c5ed5b0
Prueba <a_herrerajol@globalhitss.com>
2020-12-22 11:08:57 -0600

    demo

---
+++ src/com/company/DemoAnnotations.java
@@ -0,0 +1,13 @@
+package com.company;
+
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class DemoAnnotations
+{
+    @BeforeMethod
+    public void setUp(){
+
+
+    }
+}
